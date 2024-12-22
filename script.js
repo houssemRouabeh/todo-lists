f33ea6d73620a31678be4bd9e10c89c0d6ee7eec
unknown <haboubidalel7@gmail.com>
2024-12-22 11:32:20 +0100

    reccupertion des elements  dom

---
+++ script.js
@@ -0,0 +1,4 @@
+//reccuperation des elements  dom
+let input=document.getElementById("itemInput");
+let button=document.getElementById("addButton");
+let itemList=document.getElementById("itemList");
