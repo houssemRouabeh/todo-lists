01ccbbc1b6e60abc3206495b8dd456ff618b8d85
Hurricane.TN <houssem.rouabeh@toucomex.com>
2024-12-22 12:19:52 +0100

    ajouter toutes les fonctionnalités demandées

--- script.js
+++ script.js
@@ -2,3 +2,26 @@
 let input = document.getElementById("itemInput");
 let addButton = document.getElementById("addButton");
 let itemList = document.getElementById("itemList");
+
+//Fonction pour ajouter element a la liste
+addButton.addEventListener(
+  "click",
+  () => {
+    let text = input.value.trim();
+    if (text != "") {
+      let listItem = document.createElement("li");
+
+      listItem.textContent = text;
+
+      let deleteButton = document.createElement("button");
+      deleteButton.textContent = "🗑️";
+      deleteButton.addEventListener("click", () => {
+        itemList.removeChild(listItem);
+      });
+      listItem.appendChild(deleteButton);
+      itemList.appendChild(listItem);
+      input.value = "";
+    }
+  },
+  false
+);
