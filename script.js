b7713d3363620d19bf7b2cca7bcafcd54c553a51
Hurricane.TN <houssem.rouabeh@toucomex.com>
2024-12-22 11:30:55 +0100

    Recuperation de elements DOM

---
+++ script.js
@@ -0,0 +1,4 @@
+//Recuperation de elements DOM
+let input = document.getElementById("itemInput");
+let addButton = document.getElementById("addButton");
+let itemList = document.getElementById("itemList");
