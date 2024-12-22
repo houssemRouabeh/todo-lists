c23ee2451d7b5a644bf6f5cb9c1bbd398b20d390
raslen-der12 <raslenderbali77@gamil.com>
2024-12-22 11:24:26 +0000

    le resultat final

--- script.js
+++ script.js
@@ -2,4 +2,19 @@ const input = document.getElementById("itemInput");
 const addButton = document.getElementById("addButton");
 const itemList = document.getElementById("itemList");
 
-
+addButton.addEventListener("click",()=>{
+    let text = input.value.trim();
+    if (text != "") {
+        const listItem = document.createElement("li");
+        listItem.textContent = text;
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "🗑️";
+        deleteButton.addEventListener("click", () => {
+            itemList.removeChild(listItem);
+        });
+        
+        listItem.appendChild(deleteButton);
+        itemList.appendChild(listItem);
+        input.value = "";
+    }
+});
