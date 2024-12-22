56cce4013e7ceba59d4b58a02ea99f7a10853a5d
unknown <farahoueslati@gmail.com>
2024-12-22 11:31:42 +0100

    recuperation des elements

---
+++ script.js
@@ -0,0 +1,4 @@
+//recuperation des element DOM
+let input=document.getElementById("itemInput");
+let addButton=document.getElementById("addButton");
+let itemList=document.getElementById("itemList");
