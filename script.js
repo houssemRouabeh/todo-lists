ec654bbc36600bc54ec11fcd7091dc51237a4b44
mahrezdahbi <140942954+mahrezdahbi@users.noreply.github.com>
2024-12-22 11:31:17 +0100

    recuperation de elelment

---
+++ script.js
@@ -0,0 +1,4 @@
+// recuperation de element DOM
+let input = document.getElementById("iteminput");
+let addbutton = document.getElementById("addButton");
+let itemlist = document.getElementById("itemlist");
