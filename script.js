50a5a0ac1f70f7e593f7cfb3de76e1456e12cbed
CHAWABIahmed <[ahmedchawabi89@gmail.com]>
2024-12-22 11:32:28 +0100

    ajouter des variables

---
+++ script.js
@@ -0,0 +1,9 @@
+// recupération des eléments DOM
+let input = document.getElementById("itemInput");
+let addButton = document.getElementById("addButton");
+let liste = document.getElementById("itemList");
+
+let ajouText = ()=> {
+    addButton.onclick()
+
+}
